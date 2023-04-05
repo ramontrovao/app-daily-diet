b5731f80a4c95fe05c89766ba3ffe02d2ec06fcd
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-04-04 23:27:20 -0300

    feat: added UUID v4 to meals storage

--- src/storage/meals/createMeal/index.tsx
+++ src/storage/meals/createMeal/index.tsx
@@ -1,10 +1,29 @@
-import AsyncStorage from "@react-native-async-storage/async-storage/lib/typescript/AsyncStorage";
 import { MEALS_COLLECTION } from "@storage/config";
+
 import { getAllMeals } from "../getAllMeals";
+
 import { MealDTO } from "../MealDTO";
 
-export const createMeal = async (newMeal: MealDTO): Promise<void> => {
+import AsyncStorage from "@react-native-async-storage/async-storage/lib/typescript/AsyncStorage";
+import { v4 } from "uuid";
+
+export const createMeal = async ({
+  name,
+  description,
+  date,
+  hour,
+  isOnDiet,
+}: MealDTO): Promise<void> => {
   try {
+    const newMeal = {
+      id: v4(),
+      name,
+      description,
+      date,
+      hour,
+      isOnDiet,
+    };
+
     const mealsStoraged = await getAllMeals();
     const mealsUpdated = [...mealsStoraged, newMeal];
 
