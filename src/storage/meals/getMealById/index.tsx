b6b040aa116da899961c1145f0613c35b78c4f0a
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-04-08 22:17:56 -0300

    feat: added react-native-text-input-mask

--- src/storage/meals/getMealById/index.tsx
+++ src/storage/meals/getMealById/index.tsx
@@ -1,9 +1,14 @@
+import { AppException } from "@utils/AppException";
 import { getAllMeals } from "../getAllMeals";
 
 export const getMealById = async (id: string) => {
   try {
     const mealsStoraged = await getAllMeals();
     const mealFound = mealsStoraged.find((meal) => meal.id === id);
+
+    if (!mealFound) {
+      throw new AppException("Refeição não encontrada.");
+    }
 
     return mealFound;
   } catch (error) {
